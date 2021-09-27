e91ae8dd3104b2fd373a0b7da652ffd4c1c4cdb2
somestupidelf <austin404ellis@gmail.com>
2021-09-26 23:32:15 -0700

    Survey V2

--- app.py
+++ app.py
@@ -8,90 +8,77 @@ app.config['SECRET_KEY'] = "chickenz"
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 # debug = DebugToolBarExtension(app)
 
-cookie_survey = Survey('Rate the cookie you just had', 'give a yes, no, a number, or written response', [
-    Question('Do you like cookies?'),
-    Question('Are cookies your favorite treat?'),
-    Question('On a scale from 1 - 5 (5 being best) how good was your cookie?'),
-    Question('Write out a 20 page paper on how much you love or hate cookies')
-])
-
-# RESPONSES = []
-
-RESPONSE = {}
+RESPONSES_KEY = ""
 
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', survey=survey)
 
-@app.route('/questions/0')
-def question_zero():
-    RESPONSE['0'] = 'NOTHING'
-    question = cookie_survey.questions[0].question
-    title = cookie_survey.title
-    instructions = cookie_survey.instructions
-    return render_template('questionZero.html', question=question, title=title, instructions=instructions)
+@app.route('/start', methods=["POST"])
+def start():
+    session[RESPONSES_KEY] = []
+    return redirect('/questions/0')
 
-@app.route('/questions/1')
-def question_one():
-    if '0' in RESPONSE:
-        RESPONSE['1'] = 'NOTHING'    
-        question = cookie_survey.questions[1].question
-        title = cookie_survey.title
-        instructions = cookie_survey.instructions
-        answer = request.args.get('answer')
-        if answer == None:
-            flash('must give an answer')
-            return redirect('/questions/0')
-        else:
-            RESPONSE['0'] = answer
-            return render_template('questionOne.html', question=question, title=title, instructions=instructions)
+@app.route('/answer', methods=['POST'])
+def handle_question():
+    # taken from solution
+    choice = request.form['answer']
+
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if(len(responses) == len(survey.questions)):
+        return redirect("/complete")
     else:
-        flash('Cannot skip!')
-        return redirect('/questions/0')
+        return redirect(f"/questions/{len(responses)}")
 
-@app.route('/questions/2')
-def question_two():
-    if '1' in RESPONSE:
-        RESPONSE['2'] = 'NOTHING'
-        question = cookie_survey.questions[2].question
-        title = cookie_survey.title
-        instructions = cookie_survey.instructions
-        answer = request.args.get('answer')
-        if answer == None:
-            flash('must give an answer')
-            redirect('/questions/1')
-        else:
-            RESPONSE['1'] = answer
-            return render_template('questionTwo.html', question=question, title=title, instructions=instructions)
-    else:
-        flash('Cannot skip!')
-        return redirect('/questions/1')
+@app.route('/questions/<int:qid>')
+def show_questions(qid):
+    responses = session.get(RESPONSES_KEY)
 
-@app.route('/questions/3')
-def question_three():
-    if '2' in RESPONSE:
-        RESPONSE['3'] = 'NOTHING'
-        question = cookie_survey.questions[3].question
-        title = cookie_survey.title
-        instructions = cookie_survey.instructions
-        answer = request.args.get('answer')
-        if  answer == None:
-            flash('must give an answer')
-            redirect('/questions/2')
-        else:
-            RESPONSE['2'] = answer
-            return render_template('questionThree.html', question=question, title=title, instructions=instructions)
-    else:
-        flash('Cannot skip!')
-        return redirect('/questions/2')
+    if (responses is None):
+        return redirect('/')
+    
+    if(len(responses) len(survey.questions)):
+        return redirect("/complete")
+    
+    if(len(responses) != qid):
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
 
-@app.route('/thankYou')
-def finish():
+    question = survey.questions[qid]
+    return render_template("question.html", question_num=qid, question=question)
 
-    answer = request.args.get('answer')
-    if  answer == None:
-        flash('must give an answer')
-        redirect('/questions/3')
-    else:
-        RESPONSE['3'] = answer
-        return render_template('thankYou.html')
+@app.route("/complete")
+def complete():
+    return render_template('thankYou.html')
+
+# @app.route('/questions/3')
+# def question_three():
+#     if '2' in RESPONSE:
+#         RESPONSE['3'] = 'NOTHING'
+#         question = cookie_survey.questions[3].question
+#         title = cookie_survey.title
+#         instructions = cookie_survey.instructions
+#         answer = request.args.get('answer')
+#         if  answer == None:
+#             flash('must give an answer')
+#             redirect('/questions/2')
+#         else:
+#             RESPONSE['2'] = answer
+#             return render_template('questionThree.html', question=question, title=title, instructions=instructions)
+#     else:
+#         flash('Cannot skip!')
+#         return redirect('/questions/2')
+
+# @app.route('/thankYou')
+# def finish():
+
+#     answer = request.args.get('answer')
+#     if  answer == None:
+#         flash('must give an answer')
+#         redirect('/questions/3')
+#     else:
+#         RESPONSE['3'] = answer
+#         return render_template('thankYou.html')
